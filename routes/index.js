bf5651f979b7559fa0407c04597660424ac72ee4
TonyQ <tonylovejava@gmal.com>
2017-07-19 18:15:46 +0800

    apply form

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var TITLE = "2017 燈會志工系統";
 
+var model = require("./../model/volunteer");
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: TITLE});
@@ -19,7 +22,14 @@ router.get("/join/single",(req,res,next)=>{
 router.post("/join/single_submiting",(req,res,next)=>{
 
   //請修改這裡
+
+  
   console.log(req.body);
+
+  // model.insert_item({
+  //     name:"test",
+  //     sex:"1"
+  // });
   res.send(req.body);
   // res.render('join_single', { title: '個人報名 | 加入志工 | ' + TITLE });
 });
