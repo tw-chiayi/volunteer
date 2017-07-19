01683271369e9fcd36c68f735a66f84ab7ad58f4
Brian <brian21xe37@gmail.com>
2017-07-19 23:14:31 +0800

    新增報名表單(4修)

--- routes/index.js
+++ routes/index.js
@@ -23,14 +23,21 @@ router.post("/join/single_submiting",(req,res,next)=>{
 
   //請修改這裡
 
+  //"Name":"123","IdNumber":"","Birthday":"","Address":"","TelephoneNumber
   
-  console.log(req.body);
+  return res.send(req.body);
 
-  // model.insert_item({
-  //     name:"test",
-  //     sex:"1"
-  // });
-  res.send(req.body);
+  var { name,sex,identify,birthday,address,telephonenumber,mobilenumber,email,expertise,group,occupation,hasbook,dates,time,day } = req.body;
+
+  console.log([ Name,IdNumber,Birthday,Address]);
+
+  model.insert_item({
+      name:Name,
+      identify:IdNumber,
+      birthday:Birthday,
+      address:Address
+  });
+  res.send("送出完成:"+Name);
   // res.render('join_single', { title: '個人報名 | 加入志工 | ' + TITLE });
 });
 
