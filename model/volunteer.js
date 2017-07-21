87f3d5697992bde138b3ded627cd8a4e7e5fe71b
Brian <brian21xe37@gmail.com>
2017-07-21 20:23:31 +0800

    新增團體報名表單(1修)

--- model/volunteer.js
+++ model/volunteer.js
@@ -7,7 +7,9 @@ var build = (d) => d ;
 module.exports = {
   insert_item(data){
     //{name,sex,birthday,address,telephonenumber,mobilenumber,email,expertise,group,occupation,YesNo,Dates,Time,Day,identify}
-    return knex("form_join").insert(data).then(build);
+    return knex("form_join").insert(data).then(build);},
     
+    insert_group_item(data){
+    return knex("form_join_group").insert(data).then(build);
   }
 };
